df1552e05f26e895ddda4281eb5c42d181ae42db
Rabika-910 <rabikajothiganesh@gmail.com>
2023-11-30 22:16:19 +0530

    This is the example of local and static variable

---
+++ Ageproject/src/agepack/Age.java
@@ -0,0 +1,14 @@
+package agepack;
+
+public class Age {
+       static int age=19;
+  public static void main(String[] args) {
+	   System.out.println(age);
+	   int b=90;
+	   System.out.println(b);
+  }
+  public static void display() {
+	  System.out.println(age);
+  }
+
+}
