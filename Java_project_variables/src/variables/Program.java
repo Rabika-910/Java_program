53bca45b75e15dc525254c6309ffd99a2f5fe3bb
Rabika-910 <rabikajothiganesh@gmail.com>
2023-12-10 20:11:51 +0530

    This is the example program for Global variables and local variables

---
+++ Java_project_variables/src/variables/Program.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class Program {
+	
+		static int a=10;//global variable
+		public static void main(String[] args) {
+			System.out.println(a);
+			int b=25;//local variable
+			System.out.println(b);
+		
+		}
+}
